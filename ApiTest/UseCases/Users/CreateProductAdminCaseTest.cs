58410dfb213d5b87bef32ac34213eff172a38c15
David TA <td.ta.david@gmail.com>
2025-08-31 18:57:53 +0200

    test: wip CreateProductAdminCase

---
+++ ApiTest/UseCases/Users/CreateProductAdminCaseTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiTest.UseCases.Users
+{
+    internal class CreateProductAdminCaseTest
+    {
+        private readonly CreateProductAdminCase _adminCase;
+
+        public CreateProductAdminCaseTest()
+        {
+        }
+
+        public void ExecuteAsync_WithValidCommand_ShouldReturnSuccess()
+        {
+            // Arrange
+            var command = new CreateProductCommand("ProductName", "Description", "Price", "Season", "UsageContexte", "FlowerType", "FlowerCareAdvice");
+        }
+    }
+}
