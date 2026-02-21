cb68ef599679158c96386daca1cb0eb7a3d6bbaf
David <td.ta.david@gmail.com>
2026-02-21 19:43:34 +0100

    features: wip routing, home+login wip, register done

--- AngularUI/src/app/app.routes.ts
+++ AngularUI/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Login } from '@features/auth/pages/login/login';
+import { Register } from '@features/auth/pages/register/register';
+import { Home } from '@features/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'home', component: Home },
+  {path: 'register' , component: Register },
+  {path: 'login', component: Login },
+  {path: '', redirectTo: 'home', pathMatch: 'full' }
+];
---
+++ AngularUI/src/app/features/auth/pages/login/login.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hf-login',
+  imports: [],
+  templateUrl: './login.html',
+  styleUrl: './login.scss',
+})
+export class Login {
+
+}
