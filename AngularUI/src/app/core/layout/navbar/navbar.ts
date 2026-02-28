57c1ec3d99689a0a30199e5ab5c6190d6be9b830
David <td.ta.david@gmail.com>
2026-02-28 06:30:25 +0100

    feature: wip navbar

--- AngularUI/src/app/core/layout/navbar/navbar.ts
+++ AngularUI/src/app/core/layout/navbar/navbar.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'hf-navbar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, MatIcon],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
