c260a878526742082a8fe7ba0cc00881fea6ac5c
David TA <td.ta.david@gmail.com>
2026-04-03 23:37:10 +0200

    feature/api: value object Role for authorization

---
+++ API/Domain/ValueObjects/Role.cs
@@ -0,0 +1,15 @@
+﻿namespace API.Domain.ValueObjects;
+
+public class Role
+{
+    string Value { get; }
+    
+    private Role(string value)
+    {
+        Value = value;
+    }
+    
+    public static readonly Role Customer = new("Customer");
+    public static readonly Role Florist = new("Florist");
+    public static readonly Role Manager = new("Manager");
+}
